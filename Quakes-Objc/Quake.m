2583e1d0a7ab56d8fc20ab597aae644db594083f
Jessie <jessie.griffin@icloud.com>
2020-05-25 14:31:06 -0400

    Decoded almost all the properties

--- Quakes-Objc/Quake.m
+++ Quakes-Objc/Quake.m
@@ -15,6 +15,8 @@
                              time:(NSDate *)time
                          latitude:(double)latitude
                         longitude:(double)longitude
+                             type:(NSString *)type
+                            alert:(NSString *)alert
 {
     if (self = [super init]) {
         _magnitude = magnitude;
@@ -22,13 +24,51 @@
         _time = time;
         _latitude = latitude;
         _longitude = longitude;
+        _type = type.copy;
+        _alert = alert.copy;
     }
     return self;
 }
 
 - (instancetype)initWithDictionary:(NSDictionary *)dictionary
 {
-    return nil;
+
+    NSDictionary *properties = [dictionary objectForKey:@"properties"];
+    if (![properties isKindOfClass:[NSDictionary class]]) return nil;
+
+    NSNumber *magnitudeNumber = [properties objectForKey:@"mag"];
+    if (![magnitudeNumber isKindOfClass:[NSNumber class]]) return nil;
+
+    NSString *place = [properties objectForKey:@"place"];
+    if (![place isKindOfClass:[NSString class]]) return nil;
+
+    NSNumber *timeInMilliseconds = [properties objectForKey:@"time"];
+    if (![timeInMilliseconds isKindOfClass:[NSNumber class]]) return nil;
+    NSDate *time = [NSDate dateWithTimeIntervalSince1970:timeInMilliseconds.longValue/1000.0];
+
+    NSDictionary *geometry = [dictionary objectForKey:@"geometry"];
+    if (![geometry isKindOfClass:[NSDictionary class]]) return nil;
+
+    NSArray *coordinates = [geometry objectForKey:@"coordinates"];
+    if (![coordinates isKindOfClass:[NSArray class]]) return nil;
+    NSNumber *latitudeNumber = nil;
+    NSNumber *longitudeNumber = nil;
+
+    if (coordinates.count >= 2) {
+        latitudeNumber = [coordinates objectAtIndex:0];
+        longitudeNumber = [coordinates objectAtIndex:1];
+        if (![latitudeNumber isKindOfClass:[NSNumber class]]) return nil;
+        if (![longitudeNumber isKindOfClass:[NSNumber class]]) return nil;
+    }
+
+
+    return [self initWithMagnitude:magnitudeNumber.doubleValue
+                             place:place
+                              time:time
+                          latitude:latitudeNumber.doubleValue
+                         longitude:longitudeNumber.doubleValue
+                              type:nil
+                             alert:nil];
 }
 
 @end
